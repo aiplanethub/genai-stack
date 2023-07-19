b79af90d10c7e01294bed977af426d06e68fb319
ShreehariVaasishta <shreehari@aiplanet.com>
2023-07-19 11:42:18 +0530

    Initial setup for models

---
+++ llaim/model/base.py
@@ -0,0 +1,14 @@
+from typing import Any, Optional
+
+
+class BaseModelServer:
+    vector_store: Optional[Any] = None
+
+    def get_vector_query(self, query_type: str = "similarity"):
+        pass
+
+    def load(self, model_path: str):
+        self.model_path = model_path
+
+    def predict(self, query: Any):
+        pass
