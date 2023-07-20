06912503bb082452f8a40051a97cdc58522c9396
ShreehariVaasishta <shreehari@aiplanet.com>
2023-07-20 18:13:51 +0530

    Add a test for llamahub loader etl

--- tests/test_etl.py
+++ tests/test_etl.py
@@ -6,6 +6,7 @@
 import unittest
 
 from llaim.etl.lang_loader import list_langchain_loaders, LangLoaderEtl
+from llaim.etl.llamahub_loader import LLamaHubEtl
 
 
 class TestEtl(unittest.TestCase):
@@ -20,4 +21,8 @@ class TestEtl(unittest.TestCase):
 
     def test_langloader_etl_custom_embedding(self):
         langhub_etl = LangLoaderEtl(config="assets/config_custom_embedding.json")
+        langhub_etl.run()
+
+    def test_llamahub_loader_etl(self):
+        langhub_etl = LLamaHubEtl(config="assets/config.json")
         langhub_etl.run()
