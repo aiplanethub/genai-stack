6fa6a4963befdb9205fc53347bc31a4dce60fe1b
ShreehariVaasishta <shreehari@aiplanet.com>
2023-07-20 15:06:48 +0530

    Add loading using llamahub loaders

--- llaim/etl/llamahub_loader.py
+++ llaim/etl/llamahub_loader.py
@@ -1,6 +1,89 @@
+from typing import List
+
+from langchain.docstore.document import Document as LangDocument
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Weaviate
+from llama_index import download_loader
+
 from llaim.etl.base import EtlBase
+from llaim.utils.extraction import (
+    extract_class_init_attrs,
+    extract_method_params,
+)
+from llaim.utils.importing import import_class
 
 
 class LLamaHubEtl(EtlBase):
     def __init__(self, name: str = "LLamaHubEtl", config: str = None) -> None:
         super().__init__(name, config)
+
+    def params_for_load_data(self, kls, method):
+        return extract_method_params(kls, method)
+
+    def params_for_class_init(self, kls):
+        return extract_class_init_attrs(kls)
+
+    def load_from_source(self):
+        source = self.config_dict.get("source")
+        print("source config done")
+        LoaderCls = download_loader(source.get("name"))
+        print(LoaderCls, "<<<<<")
+
+        sanitized_init_params = {}
+        init_params = self.params_for_class_init(LoaderCls)
+        self._sanitize_params_dict(init_params, source, sanitized_init_params)
+
+        sanitized_ld_params = {}
+        ld_params = self.params_for_load_data(LoaderCls, "load_data")
+        self._sanitize_params_dict(ld_params, source, sanitized_ld_params)
+        loader = LoaderCls(**sanitized_init_params)
+        self.documents = loader.load_data(**sanitized_ld_params)
+
+        # Convert struct to LangChain document format
+        self.documents = [d.to_langchain_format() for d in self.documents]
+
+        return self.documents
+
+    def _sanitize_params_dict(self, params_dict, source_dict, sanitized_dict):
+        params_dict.pop("args", None)
+        params_dict.pop("kwargs", None)
+        for key, val in params_dict.items():
+            param_val = source_dict.get("fields", {}).get(key)
+            if val and param_val or not val:
+                sanitized_dict[key] = param_val
+
+    def _get_embedding(self):
+        destination = self.config_dict.get("destination", {})
+        if embedding := destination.get("embedding"):
+            embedding_cls = import_class(
+                f"langchain.embeddings.{embedding.get('name')}",
+            )
+            self.embedding = embedding_cls(**embedding.get("fields"))
+        elif not self.embedding:
+            self.embedding = OpenAIEmbeddings()
+        return self.embedding
+
+    def load_into_destination(self, source_docs: List[LangDocument]):
+        destination = self.config_dict.get("destination")
+
+        class_name = destination.get("class_name") or "llaim"
+        class_name = class_name.capitalize()
+
+        Weaviate.from_documents(
+            source_docs,
+            self._get_embedding(),
+            weaviate_url=destination.get(
+                "fields",
+                {},
+            ).get("url"),
+            index_name=class_name,
+        )
+        print("Stored to vectordb")
+
+    def run(self):
+        source_docs: List[LangDocument] = self.load_from_source()
+        self.load_into_destination(source_docs=source_docs)
+
+    def run(self):
+        source_docs: List[LangDocument] = self.load_from_source()
+        self.load_into_destination(source_docs=source_docs)
