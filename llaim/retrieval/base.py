3283b7e39b4edf6d2bcedf6f0e540a8c5eb9314e
Sam Joel <samjoel3101@gmail.com>
2023-07-21 18:50:42 +0530

    Add Langchain retriever

---
+++ llaim/retrieval/base.py
@@ -0,0 +1,23 @@
+from constants import VECTOR_DB_CLIENT_MAP, VectorDB
+from config import ConfigLoader
+
+
+class BaseRetriever(ConfigLoader):
+    def __init__(self, config: str):
+        super().__init__(self.__class__.name, config)
+        self.vector_store_client = self._get_vector_store_client()
+
+    def _get_vector_store_client_class(self):
+        vector_store_client_class = VECTOR_DB_CLIENT_MAP.get(self.config.get(), None)
+        if not self.vector_store_client:
+            raise ValueError(
+                f"Client not found for the specified vectordb {self.vector_store}. Available VectorDBs are {[db.value for db in VectorDB]}"
+            )
+        return vector_store_client_class
+
+    def _get_vector_store_client(self):
+        vector_store_class = self._get_vector_store_client_class()
+        return vector_store_class(self.config)
+
+    def retrieve(self, *args, **kwargs):
+        raise NotImplementedError()
