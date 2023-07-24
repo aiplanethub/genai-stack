b3deec55dc2ea357bdf35657780faa534786a41f
ShreehariVaasishta <shreehari@aiplanet.com>
2023-07-24 13:54:18 +0530

    Create a http server class with FastAPI instead of a decorator function

--- llaim/model/server.py
+++ llaim/model/server.py
@@ -1,25 +1,69 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 
 
-def http_server(route="/predict", method="POST"):
-    def decorator(func):
-        app = FastAPI()
+class HttpServer:
+    def predict(self, data=None):
+        print(data)
+        raise NotImplementedError
 
-        method_decorator = getattr(app, method.lower())
+    async def predict_api(self, request: Request) -> JSONResponse:
+        # Accessing request data
+        request_body = await request.body()
 
-        @method_decorator(route)
-        async def wrapper():
-            return func()
+        response_data = self.predict(request_body)
+        return JSONResponse(content=response_data)
 
-        # Run the FastAPI app with uvicorn
-        def run_with_uvicorn(*args, **kwargs):
-            uvicorn.run(app, *args, **kwargs)
+    def run_http_server(self):
+        self.app = FastAPI(
+            title="LLAIM Model Server",
+            description="LLAIM HTTP Model Server using FastAPI",
+        )
+        app: FastAPI = self.app
 
-        # Include the main code inside the decorator
-        if __name__ == "__main__":
-            # Run the FastAPI app with uvicorn if this module is the main module
-            run_with_uvicorn(host="127.0.0.1", port=8000)
+        app.post("/predict")(self.predict_api)
 
-    # Return the wrapped function (optional, but usually useful for other purposes)
-    return decorator
+        uvicorn.run(app, host="127.0.0.1", port=8082)
+
+
+# import asyncio
+
+# import uvicorn
+# from fastapi import FastAPI, Request
+# from fastapi.responses import JSONResponse
+
+
+# class HttpServer:
+#     def __init__(self):
+#         self.version = "0.0.1"
+
+#         self.app = FastAPI(
+#             title="LLAIM Model Server",
+#             description="LLAIM HTTP Model Server using FastAPI",
+#         )
+#         self.serving_task: Optional[asyncio.Task] = None
+
+#     async def serve(self):
+#         app: FastAPI = self.app
+
+#         @app.post("/version")
+#         async def predict(request: Request) -> JSONResponse:
+#             # Accessing request data
+#             request_body = await request.json()
+
+#             return JSONResponse(
+#                 {
+#                     "Request body": request_body,
+#                 }
+#             )
+
+#         # serve
+#         config = uvicorn.Config(app, host="127.0.0.1", port=8082)
+#         server = uvicorn.Server(config)
+#         await server.serve()
+
+
+# if __name__ == "__main__":
+#     instance = HttpServer()
+#     asyncio.run(instance.serve())
