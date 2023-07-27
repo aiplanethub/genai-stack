55897fd5732fb27be816146fca812a77a1ad9245
Sam Joel <samjoel3101@gmail.com>
2023-07-27 16:30:46 +0530

    Add langchain retrievalQA chain for retrieval

--- llaim/model/prompts/basic_qa.py
+++ llaim/model/prompts/basic_qa.py
@@ -1,16 +1,13 @@
 from langchain import PromptTemplate
 
 template = """
-Context:
+Use the following pieces of context to answer the question enclosed within  3 backticks at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Please provide an answer which is factually correct and based on the information retrieved from the vector store.
+Please also mention any quotes supporting the answer if any present in the context supplied within two double quotes "" .
 {context}
 
-Based on the information provided in Context, please answer the following Prompt:
-
-Prompt:
-{user_prompt}
-
-Please limit your response to the content available in the provided documents. If the answer cannot be determined or is uncertain based on the information given, please acknowledge that you cannot provide a definitive solution with the available data.
-Your objective is to be as accurate as possible while strictly adhering to the content within the three documents and avoiding the generation of information not explicitly present in them.
+QUESTION:```{question}```
+ANSWER:
 """
 
-BASIC_QA = PromptTemplate.from_template(template)
+BASIC_QA = PromptTemplate(template=template, input_variables=["context", "question"])
