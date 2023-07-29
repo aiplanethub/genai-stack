ec1c295808374fc2448bc314c310ca3e22dedecc
ShreehariVaasishta <shreehari@aiplanet.com>
2023-07-29 19:41:35 +0530

    Add support for custom models

--- llm_stack/model/__init__.py
+++ llm_stack/model/__init__.py
@@ -1,4 +1,4 @@
 from .server import HttpServer
 from .base import BaseModel
 from .gpt3_5 import OpenAIGpt35Model
-from .run import list_supported_models, get_model_class, AVAILABLE_MODEL_MAPS
+from .run import list_supported_models, get_model_class, AVAILABLE_MODEL_MAPS, run_custom_model
