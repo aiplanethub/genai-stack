7a6b12fe065d7da4dce04ef0fd516fc7fa59eed7
ShreehariVaasishta <shreehari@aiplanet.com>
2023-07-30 21:50:47 +0530

    Add a command to download and install airbyte

--- llm_stack/utils/run.py
+++ llm_stack/utils/run.py
@@ -1,18 +1,37 @@
+import os
 import subprocess
-from llm_stack import llm_stack_DEBUG
+from typing import List
 
 
-def run_terminal_commands(command: str, stream_output: bool = llm_stack_DEBUG):
-    process = subprocess.Popen(
-        command,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT,
-        universal_newlines=True,
-    )
+def run_terminal_commands(commands: List[str], stream_output: bool = False):
+    try:
+        result = subprocess.run(
+            commands,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT,
+            text=True,  # Specify the encoding explicitly (Python 3.7+)
+            check=True,  # Raise an exception if the subprocess returns non-zero exit code
+        )
 
-    if stream_output:
-        for line in process.stdout:
-            print(line, end="")
+        if stream_output:
+            print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing command: {e}")
+        print(f"Command output:\n{e.output}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-    process.wait()  # Wait for the subprocess to complete
+
+def execute_command_in_directory(target_directory, commands: List[List[str]]):
+    try:
+        os.makedirs(target_directory, exist_ok=True)
+        os.chdir(target_directory)
+        print(f"Current working directory: {os.getcwd()}")
+
+        # Run the provided commands here
+        for cmd in commands:
+            run_terminal_commands(cmd)
+    except FileNotFoundError as e:
+        print(f"Directory not found: {target_directory}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
