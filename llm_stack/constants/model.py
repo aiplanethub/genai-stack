6d2452057d8696e53d1929426fcddb837704af69
ShreehariVaasishta <shreehari@aiplanet.com>
2023-08-01 11:15:03 +0530

    Add gpt4all model

--- llm_stack/constants/model.py
+++ llm_stack/constants/model.py
@@ -4,9 +4,11 @@ MODEL_CONFIG_KEY = "model"
 
 class Models:
     GPT_35 = "gpt3.5"
+    GPT4ALL = "Gpt4All"
 
 
 AVAILABLE_MODEL_MAPS = {
     # Model Name: "file_name/class_name"
     Models.GPT_35: "gpt3_5/OpenAIGpt35Model",
+    Models.GPT_35: "gpt4all/Gpt4AllModel",
 }
--- llm_stack/model/__init__.py
+++ llm_stack/model/__init__.py
@@ -2,3 +2,4 @@ from .server import HttpServer
 from .base import BaseModel
 from .gpt3_5 import OpenAIGpt35Model
 from .run import list_supported_models, get_model_class, AVAILABLE_MODEL_MAPS, run_custom_model
+from .gpt4all import Gpt4AllModel
