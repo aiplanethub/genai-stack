dfb6148ccac6640bc87e1a9302f37f0c2e3e547f
Sam Joel <samjoel3101@gmail.com>
2023-08-02 15:17:38 +0530

    Render chat history from llm stack

--- ui/app/main.py
+++ ui/app/main.py
@@ -1,11 +1,10 @@
 import streamlit as st
-from app.services import get_chat_history
+from services import get_chat_history
 
 st.title("LLM Stack chatbot")
 
 chat_history = get_chat_history()
-print(chat_history)
 # Display chat messages from history on app rerun
-for message in st.session_state.messages:
+for message in chat_history:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
