d17561dfd529ff9c74ba35b62a5bef2a8ad21167
ShreehariVaasishta <shreehari@aiplanet.com>
2023-08-02 15:17:36 +0530

    Add cli commands to list and run a model as http server

--- llaim/cli.py
+++ llaim/cli.py
@@ -3,7 +3,7 @@ import sys
 import click
 
 from llaim import __version__
-
+from llaim.model.run import list_supported_models, get_model_class
 
 BANNER = """
 ██╗     ██╗      █████╗ ██╗███╗   ███╗
@@ -23,13 +23,40 @@ def main():
 
 @main.command()
 def version():
+    """Outputs version of the installed package
+
+    `llaim version`
+    """
     click.echo(f"\nLLAIM Version - {__version__}")
 
 
 @main.command()
-@click.option("-destination", help="Download and Install Airbyte", type=str)
-def dli_airbyte(destination):
-    click.echo("Downloading and installing Airbyte")
+def list_models():
+    """Lists available prebuilt models
+
+    llaim list-models
+    """
+    click.echo("Available List of models")
+    for indx, model in enumerate(list_supported_models()):
+        click.echo(f"{indx+1}. {model}")
+
+
+@main.command()
+@click.option("--model", help="Start a HTTP Server for the model", type=str)
+@click.option("-f", help="Config file", type=str)
+def start(model, f=None):
+    """Start a HTTP server for a model
+
+    `llaim start --model gpt3.5`
+    """
+    model_class = get_model_class(model_name=model)()
+    model_class.run_http_server()
+
+
+# @main.command()
+# @click.option("-destination", help="Download and Install Airbyte", type=str)
+# def dli_airbyte(destination):
+#     click.echo("Downloading and installing Airbyte")
 
 
 if __name__ == "__main__":
