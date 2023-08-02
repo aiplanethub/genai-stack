90f2f3d696548e7eedfd962028cbef3c125e305b
ShreehariVaasishta <shreehari@aiplanet.com>
2023-08-02 15:17:35 +0530

    refactor extraction util

--- llaim/utils/extraction.py
+++ llaim/utils/extraction.py
@@ -1,15 +1,20 @@
+from typing import Callable, Type
 import inspect
 
 
-def extract_class_init_attrs(clss):
-    # Get the signature of the __init__ method
-    init_signature = inspect.signature(clss.__init__)
-
-    # Extract the parameter names and their default values
-    init_parameters = init_signature.parameters
-
+def extract_func_params(func: Callable):
+    funcs_signature = inspect.signature(func)
+    funcs_params = funcs_signature.parameters
     return {
         name: param.default != inspect.Parameter.empty
-        for name, param in init_parameters.items()
+        for name, param in funcs_params.items()
         if name != "self"  # noqa: E501
     }  # {"<attr_name>":"<default_is_empty>"}
+
+
+def extract_class_init_attrs(clss: Type):
+    return extract_func_params(clss.__init__)
+
+
+def extract_method_params(clss: Type, method: Callable):
+    return extract_func_params(inspect.signature(getattr(clss, method)))
