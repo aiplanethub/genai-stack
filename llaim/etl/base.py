5d0cc544910facd4aff8025b554aa5dcbd0e1576
shreehari-aiplanet <shreehari@aiplanet.com>
2023-08-02 15:17:34 +0530

    Add utils for importing
    * Refactor base etl class

--- llaim/etl/base.py
+++ llaim/etl/base.py
@@ -1,13 +1,48 @@
+import json
+import logging
+from pathlib import Path
+
+from .exception import LLAIMEtlException
+
+
 class EtlBase:
     name: str
     config: str
 
-    def __init__(self, name: str, config: str) -> None:
-        super().__init__()
+    def __init__(self, name: str, config: str = None) -> None:
+        """Initializes the instances based on the name and config
+
+        Args:
+            name: A string to name the ETL class
+            config: A string that holds the json file path which contains the configs
+                    required to setup airbyte connection.
+        """
+        self.name = name
+        self.config = config
+        self.load_config()
+
+    @staticmethod
+    def _read_json_file(file_path: str):
+        with open(file_path) as file:
+            data = json.load(file)
+        return data
 
     def load_config(self):
-        """This method is used for loading the configs from the file."""
-        raise NotImplementedError
+        """Loads the configs and can set as class attrs"""
+        logging.info("Loading Configs")
+
+        f = Path(self.config)
+
+        if not f.exists():
+            raise LLAIMEtlException(
+                f"Unable to find the file. Input given - {self.config}",
+            )
+
+        try:
+            f = self._read_json_file(f.absolute())
+            self.config_dict = f
+        except json.JSONDecodeError as e:
+            raise LLAIMEtlException("Unable to read the config file.") from e
 
     def run(self):
         """This method should contain the actual logic for creating the EtL pipeline"""
