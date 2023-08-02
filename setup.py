15af09e0ad10b78a6d8c48cc78e1ae916d5d338c
ShreehariVaasishta <shreehari@aiplanet.com>
2023-08-02 15:17:38 +0530

    Update license in setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
         ],
     },
     install_requires=requirements,
-    license="BSD license",
+    license="Apache 2.0",
     long_description=readme + "\n\n" + history,
     include_package_data=True,
     keywords="llm_stack",
