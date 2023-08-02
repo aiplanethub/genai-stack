3d9cfcc64384d293d8b5c1ac485e38ac5a41408f
ShreehariVaasishta <shreehari@aiplanet.com>
2023-08-02 15:17:35 +0530

    Add basic tests

--- tests/test_llaim.py
+++ tests/test_llaim.py
@@ -27,7 +27,6 @@ class TestLlaim(unittest.TestCase):
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
-        assert 'llaim.cli.main' in result.output
-        help_result = runner.invoke(cli.main, ['--help'])
+        help_result = runner.invoke(cli.main, ["--help"])
         assert help_result.exit_code == 0
-        assert '--help  Show this message and exit.' in help_result.output
+        assert "--help  Show this message and exit." in help_result.output
