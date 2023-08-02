d34e0ffd7edb0b3126c568805ac7c46a3ab2c383
ShreehariVaasishta <shreehari@aiplanet.com>
2023-08-02 15:17:35 +0530

    Refactor Base model class and inherit HttpServer class

--- llaim/model/base.py
+++ llaim/model/base.py
@@ -1,14 +1,21 @@
 from typing import Any, Optional
+from llaim.model.server import HttpServer
 
 
-class BaseModel:
-    vector_store: Optional[Any] = None
+class BaseModel(HttpServer):
+    def __init__(
+        self,
+        model_path: Optional[str] = None,
+        vector_store: Optional[Any] = None,
+    ):
+        self.vector_store = vector_store
+        self.load(model_path=model_path)
 
     def get_vector_query(self, query_type: str = "similarity"):
         pass
 
     def load(self, model_path: str):
-        self.model_path = model_path
+        self.model = model_path
 
     def predict(self, query: Any):
-        pass
+        return self.model
