58c58e66b64270d514110eb037355ee2d60b8cf7
ShreehariVaasishta <shreehari@aiplanet.com>
2023-08-02 15:17:38 +0530

    Fix typos

--- llm_stack/constants/model.py
+++ llm_stack/constants/model.py
@@ -4,11 +4,11 @@ MODEL_CONFIG_KEY = "model"
 
 class Models:
     GPT_35 = "gpt3.5"
-    GPT4ALL = "Gpt4All"
+    GPT4ALL = "gpt4all"
 
 
 AVAILABLE_MODEL_MAPS = {
     # Model Name: "file_name/class_name"
     Models.GPT_35: "gpt3_5/OpenAIGpt35Model",
-    Models.GPT_35: "gpt4all/Gpt4AllModel",
+    Models.GPT4ALL: "gpt4all/Gpt4AllModel",
 }
