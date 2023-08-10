a95606d1b2ce5dbf658323840c9da63f2137c3ea
sunilgaddi <sunilgaddi917@gmail.com>
2023-08-10 10:41:09 +0530

    Remove old ui directory

--- ui/app/core/settings.py
+++
@@ -1,6 +0,0 @@
-from .config import app_config
-
-BACKEND_URL = app_config.get("llmstack", "backend_url")
-
-CHAT_HISTORY_URL = app_config.get("uri", "chat_history")
-PREDICT_URL = app_config.get("uri", "predict")
