f03ca7c294fda54898a4e89de61c69b2364fb803
sunilgaddi <sunilgaddi917@gmail.com>
2023-08-30 11:30:03 +0530

    Add conversationbuffermemory and refactor

--- genai_stack/memory/base.py
+++ genai_stack/memory/base.py
@@ -2,42 +2,32 @@ from genai_stack.stack.stack_component import StackComponent
 
 class BaseMemory(StackComponent):
     
-    def add_user_text(self):
-        """
-        This method is for storing the user prompt in the memory
-        """
-        if not self.mediator._stack.vectordb:
-            raise ValueError("VectorDB component is not provided, Memory component require a vectordb component.")
-        
-        vectorDB = self.mediator._stack.vectordb
-
-        vectorDB.add_user_text(self.get_user_text())
-
-    def add_model_text(self):
-        """
-        This method is for storing the model response in the memory
-        """
-        if not self.mediator._stack.vectordb:
-            raise ValueError("VectorDB component is not provided, Memory component require a vectordb component.")
-        
-        vectorDB = self.mediator._stack.vectordb
-
-        vectorDB.add_model_text(self.get_model_text())
-
     def get_user_text(self):
         """
-        This method is for getting the user prompt
+        This method returns the user query
         """
-        if not self.mediator._stack.prompt_engine:
-            raise ValueError("Prompt Engine component is not provided, Memory component require a prompt engine component.")
-        
-        return self.mediator._stack.prompt_engine.get_prompt()
+        raise NotImplementedError()
 
     def get_model_text(self):
         """
-        This method is for getting the model response
+        This method returns the model response
         """
-        if not self.mediator._stack.response_evaluator:
-            raise ValueError("Response Evaluator component is not provided, Memory component require a response evaluator component.")
-        
-        return self.mediator._stack.response_evaluator.get_response()
+        raise NotImplementedError()
+    
+    def get_text(self):
+        """
+        This method returns both user query and model response
+        """
+        raise NotImplementedError()
+    
+    def add_text(self):
+        """
+        This method stores both user query and model response
+        """
+        raise NotImplementedError()
+    
+    def get_chat_history(self):
+        """
+        This method returns the chat conversation history
+        """
+        raise NotImplementedError()
