e9a8484b1f5be4c6e29624aafe39ddc8de17c53f
sunilgaddi <sunilgaddi917@gmail.com>
2023-09-15 17:49:58 +0530

    Add enums and data models for stack component

---
+++ genai_stack/enums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class StackComponentType(Enum):
+    """All possible types a `StackComponent` can have."""
+    ETL = 'etl'
+    EMBEDDING = "embedding"
+    VECTOR_DB = "vector_db"
+    MODEL = "model"
+    PROMPT_ENGINE = "prompt_engine"
+    RETRIEVER = "retriever"
+    MEMORY = "memory"
