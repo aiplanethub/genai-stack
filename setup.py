653122b3633b33f3648f08e5e96febb06eb437cf
akshaj000 <akshajsr000@gmail.com>
2023-09-20 13:40:19 +0530

    Bump genai-stack version to 0.2.1

--- setup.py
+++ setup.py
@@ -88,6 +88,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/aiplanethub/genai-stack",
-    version="0.1.0",
+    version="0.2.1",
     zip_safe=False,
 )
