48f180904f915fd18f28ac6eaf3e89e59d009632
sunilgaddi <sunilgaddi917@gmail.com>
2023-09-20 19:45:43 +0530

    Rearrange server apis files

---
+++ genai_stack/constant.py
@@ -0,0 +1,2 @@
+# API Endpoint paths:
+API = "/api"
--- genai_stack/genai_server/routers/stack_routes.py
+++ genai_stack/genai_server/routers/stack_routes.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI, Response, status
+from fastapi import APIRouter, Response
 from typing import Dict, List, Union
 
+from genai_stack.constant import API
 from genai_stack.genai_store.sql_store import  SQLStore
 from genai_stack.genai_server.services.stack_service import StackService
 from genai_stack.genai_store.sql_store import SQLDatabaseDriver
@@ -18,27 +19,30 @@ store = SQLStore(
 
 service = StackService(store=store)
 
-app = FastAPI()
+router = APIRouter(
+    prefix=API,
+    tags=['stack']
+)
 
-@app.post('/api/stack')
+@router.post('/api/stack')
 def create_stack(stack:StackRequestModel) -> StackResponseModel:
     return service.create_stack(stack=stack)
 
-@app.get("/api/stack")
+@router.get("/api/stack")
 def list_stack() -> Dict[str, List[StackResponseModel]]:
     return service.list_stack()
 
-@app.get("/api/stack/{stack_id}") 
+@router.get("/api/stack/{stack_id}") 
 def get_stack(stack_id:int, response:Response) -> Union[StackResponseModel, NotFoundResponseModel]:
     filter = StackFilterModel(id=stack_id)
     return service.get_stack(filter, response)  
 
-@app.delete("/api/stack/{stack_id}")
+@router.delete("/api/stack/{stack_id}")
 def delete_stack(stack_id:int, response:Response) -> Union[DeleteResponseModel, NotFoundResponseModel]:
     filter = StackFilterModel(id=stack_id)
     return service.delete_stack(filter, response)   
 
-@app.patch("/api/stack/{stack_id}")
+@router.patch("/api/stack/{stack_id}")
 def update_stack(stack_id:int, stack:StackUpdateRequestModel, response:Response) -> Union[
     StackResponseModel, BadRequestResponseModel, NotFoundResponseModel]:
     filter = StackFilterModel(id=stack_id)
