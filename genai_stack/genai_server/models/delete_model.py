c6a3c15dc1fe0c506c752c6fbe71b9f3892f2fce
sunilgaddi <sunilgaddi917@gmail.com>
2023-09-20 08:40:35 +0530

    Refactor get and delete stack

--- genai_stack/genai_server/models/delete_model.py
+++ genai_stack/genai_server/models/delete_model.py
@@ -3,4 +3,4 @@ from pydantic import BaseModel
 class DeleteResponseModel(BaseModel):
     """Data Model for Delete Response."""
 
-    details:str
+    detail:str
--- genai_stack/genai_server/routers/main.py
+++ genai_stack/genai_server/routers/main.py
@@ -6,6 +6,7 @@ from genai_stack.genai_server.services.stack_service import StackService
 from genai_stack.genai_store.sql_store import SQLDatabaseDriver
 from genai_stack.genai_server.models.stack_models import StackRequestModel, StackResponseModel, StackFilterModel
 from genai_stack.genai_server.models.delete_model import DeleteResponseModel
+from genai_stack.genai_server.models.not_found_model import NotFoundResponseModel
 
 
 store = SQLStore(
@@ -27,20 +28,16 @@ def list_stack() -> Dict[str, List[StackResponseModel]]:
     return service.list_stack()
 
 @app.get("/api/stack/{stack_id}") 
-def get_stack(stack_id:int, response:Response) -> Union[StackResponseModel, Dict[str, str]]:
+def get_stack(stack_id:int, response:Response) -> Union[StackResponseModel, NotFoundResponseModel]:
     filter = StackFilterModel(id=stack_id)
-    stack = service.get_stack(filter)
-
-    if stack is None:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {"detail":"Stack not found."}
+    return service.get_stack(filter, response)
     
-    return stack
 
 @app.delete("/api/stack/{stack_id}")
-def get_stack(stack_id:int) -> DeleteResponseModel:
+def delete_stack(stack_id:int, response:Response) -> Union[DeleteResponseModel, NotFoundResponseModel]:
     filter = StackFilterModel(id=stack_id)
-    return service.delete_stack(filter)
+    return service.delete_stack(filter, response)
+    
 
 # @app.patch("/api/stack/{stack_id}")
 # def update_stack(stack:StackPatchModel,stack_id:int):
