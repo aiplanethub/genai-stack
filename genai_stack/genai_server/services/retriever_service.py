09b1d99121b90f38d92c61d1cc35c63316d40ea5
akshaj000 <akshajsr000@gmail.com>
2023-09-25 18:05:21 +0530

    Refactor utils

--- genai_stack/genai_server/services/retriever_service.py
+++ genai_stack/genai_server/services/retriever_service.py
@@ -1,7 +1,7 @@
-import json
 from genai_stack.genai_platform.services.base_service import BaseService
 from genai_stack.genai_server.models.retriever_models import RetrieverResponseModel, RetrieverRequestModel
 from genai_stack.genai_server.utils import get_current_stack
+from genai_stack.genai_server.settings.config import stack_config
 
 
 class RetrieverService(BaseService):
@@ -17,15 +17,7 @@ class RetrieverService(BaseService):
             Returns
                 documents : List[DocumentType]
         """
-        file = open("genai_stack/genai_server/config.json", "r")
-        config = json.loads(file.read())
-        file.close()
-        stack = get_current_stack(
-            stack_id=request.stack_id,
-            session_id=request.session_id,
-            session_indexes={},
-            config=config
-        )
+        stack = get_current_stack(config=stack_config)
         response = stack.retriever.retrieve(request.query)
         return RetrieverResponseModel(
             output=response['output']
--- genai_stack/genai_server/utils.py
+++ genai_stack/genai_server/utils.py
@@ -14,76 +14,49 @@ from genai_stack.constants import (
 from genai_stack.stack.stack import Stack
 
 components_mappers = {
-    "vectordb":{
-       "module_name": VECTORDB_MODULE,
-       "available_maps":AVAILABLE_VECTORDB_MAPS
+    "vectordb": {
+        "module_name": VECTORDB_MODULE,
+        "available_maps": AVAILABLE_VECTORDB_MAPS
     },
-    "memory":{
-        "module_name":MEMORY_MODULE,
-        "available_maps":AVAILABLE_MEMORY_MAPS
+    "memory": {
+        "module_name": MEMORY_MODULE,
+        "available_maps": AVAILABLE_MEMORY_MAPS
     },
-    "llm_cache":{
-        "module_name":LLM_CACHE_MODULE,
-        "available_maps":AVAILABLE_LLM_CACHE_MAPS
+    "llm_cache": {
+        "module_name": LLM_CACHE_MODULE,
+        "available_maps": AVAILABLE_LLM_CACHE_MAPS
     },
-    "model":{
-        "module_name":MODELS_MODULE,
-        "available_maps":AVAILABLE_MODEL_MAPS
+    "model": {
+        "module_name": MODELS_MODULE,
+        "available_maps": AVAILABLE_MODEL_MAPS
     },
-    "embedding":{
-        "module_name":EMBEDDING_MODULE,
-        "available_maps":AVAILABLE_EMBEDDING_MAPS
+    "embedding": {
+        "module_name": EMBEDDING_MODULE,
+        "available_maps": AVAILABLE_EMBEDDING_MAPS
     }
 }
 
+
 # Getting Component Class
-def get_component_class(component_name:str, class_name:str):
-    print("class name", class_name)
+def get_component_class(component_name: str, class_name: str):
     component_mapper = components_mappers.get(component_name)
-    
+
     module_name = component_mapper['module_name']
     # ex: genai_stack.vectordb
 
     cls_name = component_mapper['available_maps'].get(class_name)
     # ex: weaviate/Weaviate
 
-    cls_path = f"{module_name}.{cls_name.replace('/','.')}"
+    cls_path = f"{module_name}.{cls_name.replace('/', '.')}"
     # genai_stack.vectordb.weaviate.Weaviate
-    
-    print(cls_path)
 
     cls = import_class(cls_path)
-    
+
     return cls
 
 
-def initialize_stack(config:dict):
-    components = {}
-
-    print(config)
-    for component_name, component_config in config.get("components").items():
-        print(component_config)
-        cls = get_component_class(component_name, component_config.get("name"))
-        components[component_name] = cls.from_kwargs(**component_config)
-
-    stack = Stack(**components)
-    return stack
-
-
-# Get Current Stack
-# config => json (all component configurations)
-# session_indexes => user provided indexes 
-# session_id is the new one we create in db and use it to update the session metadata indexes
-
-session_indexes = {
-    "indexstore":"vectordb-index",
-    "memory":"memory-index",
-    "cache":"cache-index"
-}
-
 # Creating indexes provided by user
-def generate_indexes(stack_id:int, session_id:int, session_indexes:dict = {}) ->  dict:
-
+def generate_index_names(stack_id: int, session_id: int, session_indexes: dict = {}) -> dict:
     if not session_indexes.get("indexstore"):
         session_indexes["indexstore"] = "indexstore"
 
@@ -96,24 +69,17 @@ def generate_indexes(stack_id:int, session_id:int, session_indexes:dict = {}) ->
     template = f"{stack_id}-{session_id}"
 
     return {
-        "indexstore":f"{template}-{session_indexes.get('indexstore')}",
-        "memory":f"{template}-{session_indexes.get('memory')}",
-        "cache":f"{template}-{session_indexes.get('cache')}"
+        "indexstore": f"{template}-{session_indexes.get('indexstore')}",
+        "memory": f"{template}-{session_indexes.get('memory')}",
+        "cache": f"{template}-{session_indexes.get('cache')}"
     }
 
-def get_current_stack(stack_id:int, session_id:int, session_indexes:dict, config:dict):
 
-    stack = initialize_stack(config)
+def get_current_stack(config: dict):
+    components = {}
 
-    indexes = generate_indexes(stack_id, session_id, session_indexes)
-
-    stack.vectordb.create_index(index_name = indexes.get("indexstore"))
-    stack.vectordb.create_index(index_name = indexes.get("memory"))
-    stack.vectordb.create_index(index_name = indexes.get("cache"))
-
-    return stack, indexes
-
-
-
-
-
+    for component_name, component_config in config.get("components").items():
+        cls = get_component_class(component_name, component_config.get("name"))
+        components[component_name] = cls.from_kwargs(**component_config.get("config"))
+    stack = Stack(**components)
+    return stack
