9613f0fa2b39b5df45c833810fad091e0c7eb281
sunilgaddi <sunilgaddi917@gmail.com>
2023-09-26 11:19:56 +0530

    Refactor server

--- genai_stack/genai_server/database.py
+++ genai_stack/genai_server/database.py
@@ -1,10 +1,10 @@
 from genai_stack.genai_store.sql_store import SQLStore
-from genai_stack.genai_store.sql_store import SQLDatabaseDriver
-from genai_stack.genai_platform.settings.settings import settings
+from genai_stack.genai_server.settings.settings import settings
 
-db_url = settings.CONNECTION_STRING
+db_url = settings.DATABASE_URI
 db_name = settings.DATABASE_NAME
+db_driver = settings.DATABASE_DRIVER
 
 
 def initialize_store() -> SQLStore:
-    return SQLStore(url=db_url, driver=SQLDatabaseDriver.SQLITE, database=db_name)
+    return SQLStore(url=db_url, driver=db_driver, database=db_name)
---
+++ genai_stack/genai_server/server.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+from genai_stack.genai_server.routers import session_routes, retriever_routes, vectordb_routes
+
+
+def get_genai_server_app():
+    """Returns the app instance of FastAPI."""
+    
+    app = FastAPI(title="GenAI Stack", version="0.2.0")
+
+
+    """Add middleware if required."""
+    # app.middleware()
+
+
+    """Run Server"""
+    # to run this file locally, execute:
+    # uvicorn genai_stack.genai_platform.genai_stack_server:app --reload
+
+
+    """Connecting all the routers to app."""
+    app.include_router(session_routes.router)
+    app.include_router(retriever_routes.router)
+    app.include_router(vectordb_routes.router)
+
+    return app
+
