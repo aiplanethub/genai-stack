fc7852475d816f5358bb872a237e475282b3b1b8
sunilgaddi <sunilgaddi917@gmail.com>
2023-09-26 16:42:37 +0530

    Fix import and key in settings

--- genai_stack/genai_server/settings/settings.py
+++ genai_stack/genai_server/settings/settings.py
@@ -6,14 +6,16 @@ from genai_stack.genai_server.settings.config import path, stack_config, server_
 class Settings(BaseSettings):
     RUNTIME_PATH:str = path
     DATABASE_NAME:str = server_config.get("database","database_name")
-    DATABASE_DRIVER:str = server_config.get("database","driver_name")
+    DATABASE_DRIVER:str = server_config.get("database","database_driver")
     DATABASE_URI:Optional[str] = None
     STACK_CONFIG:dict = stack_config
 
-    @validator('DATABASE_URI', pre=True)
-    def create_database_uri(cls, values: Dict[str, Any]):
-            return f"{values.get('DATABASE_DRIVER')}:////{values.get('RUNTIME_PATH')}/{values.get('DATABASE_NAME')}"
-
+    @validator("DATABASE_URI", pre=True)
+    def assemble_database_uri(cls, v: Optional[str], values: Dict[str, Any]) -> str:
+        if isinstance(v, str):
+            return v
+        return values["DATABASE_DRIVER"] + ":///" + values["RUNTIME_PATH"] + "/" + values["DATABASE_NAME"]
+      
     class Config:
         case_sensitive = True
         # env_file = ".env"
