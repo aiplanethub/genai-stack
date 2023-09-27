074dc9bd73c31b0ab11794d9c0826cd89654a9cf
Sam Joel <samjoel3101@gmail.com>
2023-09-28 00:56:54 +0530

    Add ETLUtils

--- genai_stack/genai_server/services/etl_service.py
+++ genai_stack/genai_server/services/etl_service.py
@@ -1,3 +1,6 @@
+from sqlalchemy.orm import Session
+
+from genai_stack.genai_server.schemas.components import ETLJob
 from genai_stack.genai_platform.services.base_service import BaseService
 from genai_stack.genai_server.models.etl_models import ETLJobRequestType, ETLJobResponseType
 from genai_stack.genai_server.utils import get_current_stack
@@ -5,15 +8,18 @@ from genai_stack.genai_server.settings.config import stack_config
 
 
 class ETLService(BaseService):
-
-    def submit_job(self, data: ETLJobRequestType) -> ETLJobResponseType:
+    def submit_job(self, data: ETLJobRequestType, session) -> ETLJobResponseType:
         stack = get_current_stack(config=stack_config)
+        with Session(self.engine) as session:
+            etl_job = ETLJob(stack_session = session)
+            session.add(etl_job)
+            session.commit()
 
-        response = stack.etl.extract()
+            
 
         return ETLJobResponseType(
-            uuid=response['uuid'],
-            session_id=response['session_id'],
-            status=response['status'],
-            metadata=response['metadata']
+            uuid=response["uuid"],
+            session_id=response["session_id"],
+            status=response["status"],
+            metadata=response["metadata"],
         )
