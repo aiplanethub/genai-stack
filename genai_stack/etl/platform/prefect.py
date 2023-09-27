f67f529632ba1f6ab092e3b60a1ac7ff53f5768f
Sam Joel <samjoel3101@gmail.com>
2023-09-27 15:09:58 +0530

    Add PrefectETLPlatform

---
+++ genai_stack/etl/platform/prefect.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+from .base import BaseETLPlatform
+
+
+class PrefectPlatformConfig(BaseModel):
+    prefect_api_server: str
+
+
+class PrefectETLPlatform(BaseETLPlatform):
+    def handle_job(self, **kwargs):
+        try:
+            from prefect import flow
+        except ImportError:
+            print(
+                """
+                Prefect is not found. Install prefect with "pip install prefect==2.10.4"
+                """
+            )
+
+        @flow
+        def process_job():
+            self.stack.etl.run(**kwargs)
+        
+        process_job()
