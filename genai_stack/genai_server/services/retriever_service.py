20e816d29faed3da6e6069ba5e01907d525888f4
akshaj-aiplanet <akshaj@aiplanet.com>
2023-09-27 22:04:30 +0530

    Refactor vectordb and retriever

--- genai_stack/genai_server/services/retriever_service.py
+++ genai_stack/genai_server/services/retriever_service.py
@@ -1,5 +1,8 @@
+from sqlalchemy.orm import Session
+
 from genai_stack.genai_platform.services.base_service import BaseService
 from genai_stack.genai_server.models.retriever_models import RetrieverResponseModel, RetrieverRequestModel
+from genai_stack.genai_server.schemas import StackSessionSchema
 from genai_stack.genai_server.utils import get_current_stack
 from genai_stack.genai_server.settings.config import stack_config
 
@@ -7,18 +10,10 @@ from genai_stack.genai_server.settings.config import stack_config
 class RetrieverService(BaseService):
 
     def retrieve(self, data: RetrieverRequestModel) -> RetrieverResponseModel:
-        """
-        This method retrieves the documents from the vector database.
-
-            Args
-                session_id : int
-                query : str
-
-            Returns
-                documents : List[DocumentType]
-        """
-        stack = get_current_stack(config=stack_config)
-        response = stack.retriever.retrieve(data.query)
-        return RetrieverResponseModel(
-            output=response['output']
-        )
+        with Session(self.engine) as session:
+            stack_session = session.get(StackSessionSchema, data.session_id)
+            stack = get_current_stack(config=stack_config, session=stack_session)
+            response = stack.retriever.retrieve(data.query)
+            return RetrieverResponseModel(
+                output=response['output']
+            )
