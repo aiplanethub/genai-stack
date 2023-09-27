d005c3aaa2839beb6f390efd41d24251c40d1f03
Sam Joel <samjoel3101@gmail.com>
2023-09-28 01:23:51 +0530

    Add ETLPlatform execution inside service

--- genai_stack/etl/platform/prefect.py
+++ genai_stack/etl/platform/prefect.py
@@ -8,18 +8,20 @@ class PrefectPlatformConfig(BaseModel):
 
 
 class PrefectETLPlatform(BaseETLPlatform):
+    config_class = PrefectPlatformConfig
+
     def handle_job(self, **kwargs):
         try:
             from prefect import flow
         except ImportError:
             print(
                 """
-                Prefect is not found. Install prefect with "pip install prefect==2.10.4"
+                Prefect is not found. Install prefect with "pip install prefect==2.10.21"
                 """
             )
 
         @flow
         def process_job():
             self.stack.etl.run(**kwargs)
-        
+
         process_job()
