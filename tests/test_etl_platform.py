ec8230bc609a06cc5db4f5f042f82297418c3ee9
Sam Joel <samjoel3101@gmail.com>
2023-09-27 15:09:58 +0530

    Add PrefectETLPlatform

---
+++ tests/test_etl_platform.py
@@ -0,0 +1,30 @@
+import unittest
+from pathlib import Path
+
+from genai_stack.etl.langchain import list_langchain_loaders, LangchainETL
+from genai_stack.etl.platform.prefect import PrefectETLPlatform, PrefectPlatformConfig
+from genai_stack.embedding.utils import get_default_embeddings
+from genai_stack.vectordb.chromadb import ChromaDB
+from genai_stack.stack.stack import Stack
+
+
+
+class TestEtl(unittest.TestCase):
+    def setUp(self) -> None:
+        self.etl_loader = LangchainETL.from_kwargs(
+            name="PyPDFLoader", fields={"file_path": "/path/to/pdf"}
+        )
+        self.embedding = get_default_embeddings()
+        self.chromadb = ChromaDB.from_kwargs()
+
+        self.stack = Stack(etl = self.etl_loader, embedding = self.embedding, vectordb= self.chromadb, model = None)
+        self.etl_platform = PrefectETLPlatform(platform_config= PrefectPlatformConfig(), stack=self.stack)
+
+    def test_etl_platform(self):
+        dir = Path("/home/samjoel/Dphi/datasets/data")
+        for fp in list(dir.glob("*.pdf"))[:10]:
+            self.etl_platform.handle_job(file_path = fp)
+
+
+if __name__ == "__main__":
+    unittest.main()
