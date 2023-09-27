22a64cda57fbf191506cc37ee5f779a22718445e
Sam Joel <samjoel3101@gmail.com>
2023-09-27 21:19:16 +0530

    Fix StackSession creation

--- genai_stack/genai_server/utils.py
+++ genai_stack/genai_server/utils.py
@@ -1,4 +1,5 @@
 from genai_stack.utils import import_class
+from genai_stack.enums import StackComponentType
 from genai_stack.constants import (
     VECTORDB_MODULE,
     AVAILABLE_VECTORDB_MAPS,
@@ -13,48 +14,27 @@ from genai_stack.constants import (
     RETRIEVER_MODULE,
     AVAILABLE_RETRIEVER_MAPS,
     PROMPT_ENGINE_MODULE,
-    AVAILABLE_PROMPT_ENGINE_MAPS
+    AVAILABLE_PROMPT_ENGINE_MAPS,
 )
 
 
 components_mappers = {
-    "vectordb": {
-        "module_name": VECTORDB_MODULE,
-        "available_maps": AVAILABLE_VECTORDB_MAPS
-    },
-    "memory": {
-        "module_name": MEMORY_MODULE,
-        "available_maps": AVAILABLE_MEMORY_MAPS
-    },
-    "llm_cache": {
-        "module_name": LLM_CACHE_MODULE,
-        "available_maps": AVAILABLE_LLM_CACHE_MAPS
-    },
-    "model": {
-        "module_name": MODELS_MODULE,
-        "available_maps": AVAILABLE_MODEL_MAPS
-    },
-    "embedding": {
-        "module_name": EMBEDDING_MODULE,
-        "available_maps": AVAILABLE_EMBEDDING_MAPS
-    },
-    "prompt_engine": {
-        "module_name": PROMPT_ENGINE_MODULE,
-        "available_maps": AVAILABLE_PROMPT_ENGINE_MAPS
-    },
-    "retriever": {
-        "module_name": RETRIEVER_MODULE,
-        "available_maps": AVAILABLE_RETRIEVER_MAPS
-    }
+    "vectordb": {"module_name": VECTORDB_MODULE, "available_maps": AVAILABLE_VECTORDB_MAPS},
+    "memory": {"module_name": MEMORY_MODULE, "available_maps": AVAILABLE_MEMORY_MAPS},
+    "llm_cache": {"module_name": LLM_CACHE_MODULE, "available_maps": AVAILABLE_LLM_CACHE_MAPS},
+    "model": {"module_name": MODELS_MODULE, "available_maps": AVAILABLE_MODEL_MAPS},
+    "embedding": {"module_name": EMBEDDING_MODULE, "available_maps": AVAILABLE_EMBEDDING_MAPS},
+    "prompt_engine": {"module_name": PROMPT_ENGINE_MODULE, "available_maps": AVAILABLE_PROMPT_ENGINE_MAPS},
+    "retriever": {"module_name": RETRIEVER_MODULE, "available_maps": AVAILABLE_RETRIEVER_MAPS},
 }
 
 
 # Getting Component Class
 def get_component_class(component_name: str, class_name: str):
     component_mapper = components_mappers.get(component_name)
-    module_name = component_mapper['module_name']
+    module_name = component_mapper["module_name"]
     # ex: genai_stack.vectordb
-    cls_name = component_mapper['available_maps'].get(class_name)
+    cls_name = component_mapper["available_maps"].get(class_name)
     # ex: weaviate/Weaviate
     cls_path = f"{module_name}.{cls_name.replace('/', '.')}"
     # genai_stack.vectordb.weaviate.Weaviate
@@ -65,34 +45,27 @@ def get_component_class(component_name: str, class_name: str):
 
 
 # Creating indexes provided by user
-def generate_index_names(stack_id: int, session_id: int, session_indexes: dict = {}) -> dict:
-    if not session_indexes.get("indexstore"):
-        session_indexes["indexstore"] = "indexstore"
+def create_indexes(stack, stack_id: int, session_id: int) -> dict:
+    components = [StackComponentType.VECTOR_DB, StackComponentType.MEMORY]
 
-    if not session_indexes.get("memory"):
-        session_indexes["memory"] = "memory"
+    metadata = {}
+    for component in components:
+        index_name = f"{stack_id}_{session_id}_{component}"
+        stack.vectordb.create_index(index_name=index_name)
+        metadata[component] = {"index_name": index_name}
 
-    if not session_indexes.get("cache"):
-        session_indexes["cache"] = "cache"
-
-    template = f"{stack_id}-{session_id}"
-
-    return {
-        "indexstore": f"{template}-{session_indexes.get('indexstore')}",
-        "memory": f"{template}-{session_indexes.get('memory')}",
-        "cache": f"{template}-{session_indexes.get('cache')}"
-    }
+    return metadata
 
 
-def get_current_stack(config: dict):
+def get_current_stack(config: dict, session):
     components = {}
 
     for component_name, component_config in config.get("components").items():
         cls = get_component_class(component_name, component_config.get("name"))
         components[component_name] = cls.from_kwargs(**component_config.get("config"))
-    
+
     # To avoid circular import error
     from genai_stack.stack.stack import Stack
-    
+
     stack = Stack(**components)
     return stack
--- genai_stack/genai_store/schemas/session_schemas.py
+++ genai_stack/genai_store/schemas/session_schemas.py
@@ -2,24 +2,18 @@ from sqlalchemy import Column, Integer, JSON, ForeignKey, UUID
 
 from genai_stack.genai_store.schemas.base_schemas import TimeStampedSchema
 
+
 class StackSessionSchema(TimeStampedSchema):
     """
     SQL Schema for Stack Sessions.
 
     Args:
         stack_id : Integer
-        meta_data : JSON 
+        meta_data : JSON
     """
 
     __tablename__ = "stack_sessions"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-
-    stack_id = Column(
-        Integer,
-        ForeignKey('stacks.id', ondelete='CASCADE'),
-        nullable=False
-    )
-
-    meta_data = Column(JSON)
-
+    stack_id = Column(Integer, ForeignKey("stacks.id", ondelete="CASCADE"), nullable=False)
+    metadata = Column(JSON)
