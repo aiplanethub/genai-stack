e142ab6ee8454cf96e8299c45ae51b0aa992a04e
akshaj-aiplanet <akshaj@aiplanet.com>
2023-09-27 22:04:30 +0530

    Refactor vectordb and retriever

--- genai_stack/genai_server/services/vectordb_service.py
+++ genai_stack/genai_server/services/vectordb_service.py
@@ -1,8 +1,10 @@
+from sqlalchemy.orm import Session
 from genai_stack.genai_platform.services.base_service import BaseService
 from genai_stack.genai_server.models.vectordb_models import (
     DocumentType, RetrieverAddDocumentsRequestModel, RetrieverSearchRequestModel, RetrieverAddDocumentsResponseModel,
     RetrieverSearchResponseModel
 )
+from genai_stack.genai_server.schemas import StackSessionSchema
 from genai_stack.genai_server.utils import get_current_stack
 from genai_stack.genai_server.settings.config import stack_config
 
@@ -10,41 +12,27 @@ from genai_stack.genai_server.settings.config import stack_config
 class VectorDBService(BaseService):
 
     def add_documents(self, data: RetrieverAddDocumentsRequestModel) -> RetrieverAddDocumentsResponseModel:
-        """
-        This method adds the documents to the vector database.
 
-            Args
-                session_id : int
-                documents : List[DocumentType]
-
-            Returns
-                bool
-        """
-        stack = get_current_stack(config=stack_config)
-        stack.vectordb.add_documents(data.documents)
-        return RetrieverAddDocumentsResponseModel(documents=[
-            DocumentType(
-                page_content=document.page_content,
-                metadata=document.metadata
-            ) for document in data.documents
-        ])
+        with Session(self.engine) as session:
+            stack_session = session.get(StackSessionSchema, data.session_id)
+            stack = get_current_stack(config=stack_config, session=stack_session)
+            stack.vectordb.add_documents(data.documents)
+            return RetrieverAddDocumentsResponseModel(documents=[
+                DocumentType(
+                    page_content=document.page_content,
+                    metadata=document.metadata
+                ) for document in data.documents
+            ])
 
     def search(self, data: RetrieverSearchRequestModel) -> RetrieverSearchResponseModel:
-        """
-        This method searches the documents from the vector database.
 
-            Args
-                session_id : int
-                query : str
-
-            Returns
-                documents : List[DocumentType]
-        """
-        stack = get_current_stack(config=stack_config)
-        documents = stack.vectordb.search(data.query)
-        return RetrieverSearchResponseModel(documents=[
-            DocumentType(
-                page_content=document.page_content,
-                metadata=document.metadata
-            ) for document in documents
-        ])
+        with Session(self.engine) as session:
+            stack_session = session.get(StackSessionSchema, data.session_id)
+            stack = get_current_stack(config=stack_config, session=stack_session)
+            documents = stack.vectordb.search(data.query)
+            return RetrieverSearchResponseModel(documents=[
+                DocumentType(
+                    page_content=document.page_content,
+                    metadata=document.metadata
+                ) for document in documents
+            ])
