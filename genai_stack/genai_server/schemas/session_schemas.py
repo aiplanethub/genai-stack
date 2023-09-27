98ce203403e861b94cdb4235d3533f4e9bccbe48
Sam Joel <samjoel3101@gmail.com>
2023-09-28 00:11:18 +0530

    Add ETL model

--- genai_stack/genai_server/schemas/session_schemas.py
+++ genai_stack/genai_server/schemas/session_schemas.py
@@ -2,20 +2,18 @@ from sqlalchemy import Column, Integer, JSON
 
 from genai_stack.genai_server.schemas.base_schemas import TimeStampedSchema
 
+
 class StackSessionSchema(TimeStampedSchema):
     """
     SQL Schema for Stack Sessions.
 
     Args:
         stack_id : Integer
-        meta_data : JSON 
+        meta_data : JSON
     """
 
     __tablename__ = "stack_sessions"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-
-    stack_id = Column(Integer,  nullable=False)
-
+    stack_id = Column(Integer, nullable=False)
     meta_data = Column(JSON)
-
