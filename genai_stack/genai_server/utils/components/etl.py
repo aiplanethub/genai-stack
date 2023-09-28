bc724b503059b4205f181c53db6e232d4f08f10a
Sam Joel <samjoel3101@gmail.com>
2023-09-28 17:39:21 +0530

    Fix request parsing for etl

--- genai_stack/genai_server/utils/components/etl.py
+++ genai_stack/genai_server/utils/components/etl.py
@@ -1,6 +1,6 @@
 import os
 
-from genai_stack.genai_server.settings import settings
+from genai_stack.genai_server.settings.settings import settings
 from genai_stack.genai_server.models.etl_models import ETLJobRequestType
 from genai_stack.genai_server.settings.config import stack_config
 
@@ -15,8 +15,8 @@ DATA_DIR = "data"
 
 
 class ETLUtil:
-    def __init__(self, data: ETLJobRequestType):
-        self.data = ETLJobRequestType
+    def __init__(self, data):
+        self.data = data
         self.data_dir = os.path.join(settings.RUNTIME_PATH)
         self._setup_data_dir()
 
@@ -43,6 +43,6 @@ def get_etl_platform(**kwargs):
     etl_platform, config = list(etl_platform_config.items())[0]
 
     cls_name = AVAILABLE_ETL_PLATFORMS.get(etl_platform)
-    cls = f"{ETL_PLATFORM_MODULE}.{cls_name.replace('/', '.')}"
+    cls = import_class(f"{ETL_PLATFORM_MODULE}.{cls_name.replace('/', '.')}")
 
     return cls(platform_config=cls.config_class(**config), **kwargs)
