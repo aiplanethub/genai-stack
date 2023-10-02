ad885c8d727585e666d6da8150e3172192b5bc1f
akshaj000 <akshajsr000@gmail.com>
2023-10-02 14:43:40 +0530

    Add load dataset method

--- genai_stack/fine_tune/llama27b.py
+++ genai_stack/fine_tune/llama27b.py
@@ -1,50 +1,47 @@
 from genai_stack.fine_tune.base import BaseHyperParametersConfigModel, BaseFineTuneConfigModel, BaseFineTune
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, AutoTokenizer
-import torch
 from datasets import load_dataset
 import ray.data
 
-from genai_stack.fine_tune.base import BaseFineTune, BaseFineTuneConfigModel
+
+class HyperParametersConfigModel(BaseHyperParametersConfigModel):
+    pass
 
 
 class LLama27bConfigModel(BaseFineTuneConfigModel):
-    model_name: str = "daryl149/llama-2-7b-chat-hf"
-    task: str = "cola"
+
+    """
+    Data Model for the configs
+    """
+
+    instruction: str = "text-summarization"
+    hyperparameters: HyperParametersConfigModel
+    dataset: str
 
 
 class LLama27bFineTune(BaseFineTune):
+    """
+    Class for fine-tuning the model
+    """
+
     config_class = LLama27bConfigModel
 
-    def load_model(self, model_name: str):
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
-            bnb_4bit_use_double_quant=True,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=torch.bfloat16,
-        )
-        torch.cuda.get_device_capability()
-        device_map = "auto"
+    def load_model(self):
+        pass
 
-        model = AutoModelForCausalLM.from_pretrained(
-            model_name,
-            quantization_config=bnb_config,
-            device_map=device_map,
-        )
-        model.config.pretraining_tp = 1
-
-        tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True, use_fast=True)
-        tokenizer.pad_token = tokenizer
-        torch.cuda.empty_cache()
-        print("model", model)
-        print("tokenizer", tokenizer)
-        return model, tokenizer
-   
-  
     def load_dataset(self):
-        datasets = load_dataset("glue", self.config.task)
+        datasets = load_dataset(self.config.dataset)
         ray_datasets = {
             "train": ray.data.from_huggingface(datasets["train"]),
             "validation": ray.data.from_huggingface(datasets["validation"]),
             "test": ray.data.from_huggingface(datasets["test"]),
         }
         return ray_datasets
+
+    def train(self):
+        pass
+
+    def tune(self):
+        pass
+
+    def evaluate(self):
+        pass
