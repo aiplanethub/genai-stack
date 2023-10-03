31a82c742119c56d0ca4c85cd47f5b58841c82be
ShreehariVaasishta <shreehari9481@gmail.com>
2023-10-03 16:09:56 +0530

    Fix a typo
    * PREBUILT_ETL_LOADERS -> AVAILABLE_ETL_LOADERS

--- genai_stack/etl/run.py
+++ genai_stack/etl/run.py
@@ -1,17 +1,17 @@
-from genai_stack.constants.etl.etl import PREBUILT_ETL_LOADERS, ETL_MODULE
+from genai_stack.constants.etl.etl import AVAILABLE_ETL_LOADERS, ETL_MODULE
 from genai_stack.utils.importing import import_class
 
 from genai_stack.core import ConfigLoader
 
 
 def list_etl_loaders():
-    return PREBUILT_ETL_LOADERS.keys()
+    return AVAILABLE_ETL_LOADERS.keys()
 
 
 def run_etl_loader(config_file: str, vectordb):
     config_cls = ConfigLoader(name="EtlLoader", config=config_file)
     etl_cls = import_class(
-        f"{ETL_MODULE}.{PREBUILT_ETL_LOADERS.get(config_cls.config.get('etl'))}".replace(
+        f"{ETL_MODULE}.{AVAILABLE_ETL_LOADERS.get(config_cls.config.get('etl'))}".replace(
             "/",
             ".",
         )
