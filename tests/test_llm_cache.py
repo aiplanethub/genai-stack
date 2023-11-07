173e8b3eccc1c2a6b2f927f6cfc383595b22bf8c
akshaj000 <akshajsr000@gmail.com>
2023-11-07 23:20:03 +0530

    Give priority to cache over memory

--- tests/test_llm_cache.py
+++ tests/test_llm_cache.py
@@ -5,6 +5,11 @@
 import unittest
 
 from genai_stack.embedding.langchain import LangchainEmbedding
+from genai_stack.etl.langchain import LangchainETL
+from genai_stack.memory import ConversationBufferMemory
+from genai_stack.model import OpenAIGpt35Model
+from genai_stack.prompt_engine.engine import PromptEngine
+from genai_stack.retriever import LangChainRetriever
 from genai_stack.stack.stack import Stack
 from genai_stack.vectordb import ChromaDB, Weaviate
 from genai_stack.llm_cache import LLMCache
@@ -27,6 +32,13 @@ class TestLLLMCache(unittest.TestCase):
         attributes=["source", "page"]
     )
     llm_cache = LLMCache.from_kwargs()
+    etl = LangchainETL.from_kwargs(
+        name="WebBaseLoader", fields={"web_path": "https://en.wikipedia.org/wiki/Artificial_intelligence"}
+    )
+    memory = ConversationBufferMemory.from_kwargs()
+    retriever = LangChainRetriever.from_kwargs()
+    llm = OpenAIGpt35Model.from_kwargs(parameters={"openai_api_key": ""})
+    prompt_engine = PromptEngine.from_kwargs()
 
     def test_llm_cache_with_chromadb(self):
         stack = Stack(
@@ -95,3 +107,30 @@ class TestLLLMCache(unittest.TestCase):
             query=query,
             metadata={"source": "Wikipdfedia", "page": 1}
         )
+
+    def test_cache_is_given_priority(self):
+        stack = Stack(
+            model=self.llm,
+            embedding=self.embedding,
+            vectordb=self.chromadb,
+            llm_cache=self.llm_cache,
+            memory=self.memory,
+            etl=self.etl,
+            retriever=self.retriever,
+            prompt_engine=self.prompt_engine
+        )
+
+        flow = [
+            "When was artificial intelligence founded as an academic discipline?",
+            "If I ask about AI again, end the sentence with XAI",
+            "When was artificial intelligence founded as an academic discipline?"
+        ]
+
+        responses = {}
+
+        for i in flow:
+            response = stack.retriever.retrieve(i)
+            print(response)
+            if i in responses:
+                assert responses[i] == response
+        print(responses)
